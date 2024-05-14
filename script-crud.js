88b520d30c573af6e89d4175fcd0c67ae062baa5
jlateles <juliatelesicm@gmail.com>
2024-05-14 15:13:57 -0300

    feat: trabalhando com manipulação de elementos

---
+++ script-crud.js
@@ -0,0 +1,11 @@
+// encontrar o botão adicionar tarefa
+
+const btnAdicionarTarefa = document.querySelector('.app__button--add-task');
+const formAdicionarTarefa = document.querySelector('.app__form-add-task');
+const textarea = document.querySelector('.app__form-textarea');
+
+btnAdicionarTarefa.addEventListener('click', () =>{
+    formAdicionarTarefa.classList.toggle('hidden')
+})
+
+formAdicionarTarefa
