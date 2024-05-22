2658e3a85d97e89c3e2d755446cc627937b0f188
jlateles <juliatelesicm@gmail.com>
2024-05-22 15:04:05 -0300

    feat: implementação onclick do botão remover concluídos

--- script-crud.js
+++ script-crud.js
@@ -1,5 +1,4 @@
 // encontrar o botão adicionar tarefa
-
 const btnAdicionarTarefa = document.querySelector('.app__button--add-task')
 const formAdicionarTarefa = document.querySelector('.app__form-add-task')
 const textarea = document.querySelector('.app__form-textarea')
@@ -7,9 +6,12 @@ const ulTarefas = document.querySelector('.app__section-task-list')
 const btnCancelar = document.querySelector('.app__form-footer__button--cancel')
 const paragrafoDescricaoTarefa = document.querySelector('.app__section-active-task-description')
 
+const btnRemoverConcluidas = document.querySelector('#btn-remover-concluidas')
 
-const tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
+
+let tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
 let tarefaSelecionada = null
+let liTarefaSelecionada = null
 
 function atualizarTarefas (){
     localStorage.setItem('tarefas', JSON.stringify(tarefas))
@@ -53,7 +55,12 @@ function criarElementoTarefa(tarefa) {
     li.append(svg)
     li.append(paragrafo)
     li.append(botao)
-    li.onclick = () =>{
+
+    if (tarefa.completa){
+        li.classList.add('app__section-task-list-item-complete')
+        botao.setAttribute('disabled', 'disabled')
+    } else {
+        li.onclick = () =>{
         document.querySelectorAll('.app__section-task-list-item-active')
             .forEach(elemento => {
                 elemento.classList.remove('app__section-task-list-item-active')
@@ -61,13 +68,17 @@ function criarElementoTarefa(tarefa) {
        if(tarefaSelecionada == tarefa){
             paragrafoDescricaoTarefa.textContent = ''
             tarefaSelecionada = null
+            liTarefaSelecionada = null
             return
        }
         tarefaSelecionada = tarefa
+        liTarefaSelecionada = li
         paragrafoDescricaoTarefa.textContent = tarefa.descricao
         
         li.classList.add('app__section-task-list-item-active')
+        }
     }
+
     return li
 }   
 
@@ -92,6 +103,25 @@ tarefas.forEach(tarefa => {
     const elementoTarefa = criarElementoTarefa(tarefa)
     ulTarefas.append(elementoTarefa)
 });
+
+document.addEventListener('FocoFinalizado', () => {
+    if (tarefaSelecionada && liTarefaSelecionada){
+        liTarefaSelecionada.classList.remove('app__section-task-list-item-active')
+        liTarefaSelecionada.classList.add('app__section-task-list-item-complete')
+        liTarefaSelecionada.querySelector('button').setAttribute('disabled', 'disabled')
+        tarefaSelecionada.completa = true
+        atualizarTarefas()
+    }
+})
+
+btnRemoverConcluidas.onclick = () =>{
+    const seletor = ".app__section-task-list-item-complete"
+    document;querySelectorAll(seletor).forEach(elemento => {
+        elemento.remove()
+    })
+    tarefas = tarefas.filter(tarefa => !tarefa.completa)
+    atualizarTarefas()
+}
 
 // função para limpar o conteúdo do textarea e esconder o formulário
 const limparFormulario = () => {
